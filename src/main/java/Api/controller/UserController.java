524bb6bc9f876c05a6c3f03a58f25650de651c45
Rachel Carvalho <rachelacarvalho@gmail.com>
2021-08-07 19:45:07 -0300

    Implementa métodos no User Controller

--- src/main/java/Api/controller/UserController.java
+++ src/main/java/Api/controller/UserController.java
@@ -3,10 +3,13 @@ package Api.controller;
 import Api.model.User;
 import Api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/user")
@@ -19,15 +22,42 @@ public class UserController {
     PasswordEncoder bCryptPasswordEncoder;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
     public User saveUser(@RequestBody User user){
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
-    public List<User> getUsers(){
+    @ResponseStatus(HttpStatus.OK)
+    public List<User> listUsers(){
         return (List<User>) userRepository.findAll();
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public User getUserById(@PathVariable("id") Integer id){
+        Optional<User> user = userRepository.findById(id);
 
+        return user.get();
+    }
+    @RequestMapping(value = "/updateProduct/{id}", method = RequestMethod.PATCH)
+    @ResponseStatus(HttpStatus.OK)
+    public void updateUser(@PathVariable Integer id, @RequestBody User user) {
+        userRepository.findById(id).map(record -> {
+            record.setName(user.getName());
+            record.setEmail(user.getEmail());
+            record.setCellphone(user.getCellphone());
+            record.setUser_photo(user.getUser_photo());
+            record.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+            User updated = userRepository.save(record);
+            return ResponseEntity.ok().body(updated);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable("id") Integer id) {
+        userRepository.deleteById(id);
+    }
 }
